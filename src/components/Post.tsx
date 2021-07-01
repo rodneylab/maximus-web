dda8b4462d32b51a1a4caaf9ea117a108c7af0a6
Rodney Lab <codesign@rodneylab.com>
2021-07-01 16:22:18 +0100

    fix(post): improved date format on created time

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,10 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import React from 'react';
 import { usePostQuery } from '../generated/graphql';
 import Videos from './Videos';
 
+dayjs.extend(relativeTime);
 const Post = ({ slug }) => {
   const { data, error, loading } = usePostQuery({
     variables: {
@@ -23,11 +26,13 @@ const Post = ({ slug }) => {
   }
 
   const { title, createdAt, videos } = data.post;
+  const createdDate = dayjs(createdAt);
+  const createdDateString = createdDate.fromNow();
   return (
     <>
       <h3>{title}</h3>
       <p>{slug}</p>
-      <p>Created: {createdAt}</p>
+      <p>Created: {createdDateString}</p>
       <h4>Videos</h4>
       <Videos slug={slug} videos={videos} />
     </>
